23385980352652b909f74513f7bd7ec007835103
grant smith <grantsmith1256@gmail.com>
2018-12-14 17:08:01 +0000

    Automatically backed up by Learn

---
+++ Worlds-Top-Yoga-Studios/lib/Worlds/Top/Yoga/Studios.rb
@@ -0,0 +1,12 @@
+require "Worlds/Top/Yoga/Studios/version"
+
+module Worlds
+  module Top
+    module Yoga
+      module Studios
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ Worlds-Top-Yoga-Studios/lib/Worlds/Top/Yoga/Studios/version.rb
@@ -0,0 +1,9 @@
+module Worlds
+  module Top
+    module Yoga
+      module Studios
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
